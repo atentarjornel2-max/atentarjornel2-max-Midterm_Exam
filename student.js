4d527d6a196aa88ab542db359666369caf10e1fb
Jornel A. Atentar <atentarjornel2@gmail.com>
2026-04-24 10:27:59 +0800

    Updated index.html (fixed links and content)

---
+++ student.js
@@ -0,0 +1,9 @@
+const name = "Jornel Atentar";
+const course = "Web Development";
+const year = "2nd Year";
+const language = "JavaScript";
+
+console.log("Name:", name);
+console.log("Course:", course);
+console.log("Year Level:", year);
+console.log("Favorite Language:", language);
